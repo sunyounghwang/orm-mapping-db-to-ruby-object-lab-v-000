e63db99db21771298a24ac727301a4078fc0ca35
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-10-16 16:32:47 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -49,6 +49,17 @@ class Student
     DB[:conn].execute(sql, 12).map { |row| new_from_db(row) }
   end
 
+  def self.first_X_students_in_grade_10(num)
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = ?
+    LIMIT ?
+    SQL
+
+    DB[:conn].execute(sql, 10, num).map { |row| new_from_db(row) }
+  end
+
   def self.find_by_name(name)
     sql = <<-SQL
     SELECT *
