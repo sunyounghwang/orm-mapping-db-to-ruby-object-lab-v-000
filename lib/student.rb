70ea2fa921fbc78fee85fd1cc53373aeac39ffae
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-10-16 16:22:03 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,9 +3,9 @@ class Student
 
   def self.new_from_db(row)
     student = new
-    @id = row[0],
-    @name = row[1],
-    @grade = row[2]
+    student.id = row[0],
+    student.name = row[1],
+    student.grade = row[2]
     student
   end
 
